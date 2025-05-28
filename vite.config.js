ceb32a7b9556201e4e8e33f876395e34f0bc3c74
Usama Khatri <usamaofficial18@gmail.com>
2025-05-28 19:19:55 +0530

    Update settings

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,23 @@ import { resolve } from 'path'
 export default {
   root: resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        about: resolve(__dirname, 'src/about.html'),
+        service: resolve(__dirname, 'src/service.html'),
+        privacy: resolve(__dirname, 'src/privacy.html'),
+        careers: resolve(__dirname, 'src/careers.html'),
+        blogs: resolve(__dirname, 'src/blogs.html'),
+         // add other HTML files here
+        contact: resolve(__dirname, 'src/contact.html'),
+      },
+    }
   },
   server: {
     port: 8080
   },
-  base:'/fynstatic/',
   // Optional: Silence Sass deprecation warnings. See note below.
   css: {
      preprocessorOptions: {
